bd07f0ddf4f9e9faca515bc2cbd5ceb4b9f7ea10
CipherGrind <101846763+CipherGrind@users.noreply.github.com>
2023-10-19 04:03:54 -0400

    v9

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const url = require('url');
 const slugify = require('slugify');
 const replaceTemplate = require('./modules/replaceTemplate');
 
+// SERVER
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
   'utf-8'
@@ -18,47 +19,48 @@ const tempProduct = fs.readFileSync(
   'utf-8'
 );
 
-const data = fs.readFileSync(`${__dirname}/data.json`, 'utf-8');
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
-const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
 console.log(slugs);
 
 const server = http.createServer((req, res) => {
-
   const { query, pathname } = url.parse(req.url, true);
 
-  //Overview Page
+  // Overview page
   if (pathname === '/' || pathname === '/overview') {
-    res.writeHead(200, { 'Content-type': 'text/html' });
+    res.writeHead(200, {
+      'Content-type': 'text/html'
+    });
 
-    const cardsHtml = dataObj
-      .map((el) => replaceTemplate(tempCard, el))
-      .join('');
-
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
     const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
     res.end(output);
 
-    //Product page
+    // Product page
   } else if (pathname === '/product') {
+    res.writeHead(200, {
+      'Content-type': 'text/html'
+    });
     const product = dataObj[query.id];
     const output = replaceTemplate(tempProduct, product);
-
     res.end(output);
 
-    //API
+    // API
   } else if (pathname === '/api') {
-
-    res.writeHead(200, { 'content-type': 'application/json' });
+    res.writeHead(200, {
+      'Content-type': 'application/json'
+    });
     res.end(data);
 
-    //Not found
+    // Not found
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
-      'my-own-header': 'hello-world',
+      'my-own-header': 'hello-world'
     });
-    res.end('<h1>page not found</h1>');
+    res.end('<h1>Page not found!</h1>');
   }
 });
 
